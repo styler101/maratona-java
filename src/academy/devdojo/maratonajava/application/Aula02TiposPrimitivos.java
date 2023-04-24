0a3e64ccb8fb905d9b46f23e94b38a99bb183ca8
Lucas <lucas.santos@mundowap.com.br>
2023-04-24 08:46:31 -0300

    declaração e tamanho em memória

--- src/academy/devdojo/maratonajava/application/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/application/Aula02TiposPrimitivos.java
@@ -4,7 +4,17 @@ public class Aula02TiposPrimitivos {
   public static void main(String[] args) {
       // Tipos primitivos
      // int, double, float, char, byte, short, long, boolean
-     int age = 10;
-     System.out.println("The age is :"+ age);
+     double salario = 2000;
+     float salarioFloat = 2508f;
+     byte idadeByte = 10; // -128 a 127
+     short idadeShort = 10;
+     boolean result = true;
+     long valorEmMilhoes = 123012401242L;
+     char caractere ='L';
+
+     String value = String.valueOf(salario);
+     System.out.println("Teste");
+
+
   }
 }
