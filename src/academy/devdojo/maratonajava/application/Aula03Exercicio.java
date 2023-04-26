a51451afde66e79fd73702079e2a804e5591404a
Lucas <lucas.santos@mundowap.com.br>
2023-04-26 06:38:20 -0300

    operadores aritmeticos

---
+++ src/academy/devdojo/maratonajava/application/Aula03Exercicio.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula03Exercicio {
+
+  public static void main(String[] args){
+    Locale.setDefault(Locale.US);
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter with your name: ");
+    String userName = scanner.nextLine();
+    System.out.println("Enter with the address: ");
+    String userAddress = scanner.nextLine();
+    System.out.println("Enter with your salary");
+    double userSalary = scanner.nextDouble();
+    scanner.nextLine();
+    System.out.println("Enter with the current date dd/mm/yyyy");
+    String userPaymentDate = scanner.nextLine();
+    System.out.printf("Eu %s , morando no endereço %s, confirmo que recebi o salário de %.2f , na data %s", userName, userAddress, userSalary, userPaymentDate);
+    scanner.close();
+  }
+
+}
---
+++ src/academy/devdojo/maratonajava/application/Aula04Operadores.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.application;
+
+public class Aula04Operadores {
+
+  public static void main(String[] args) {
+    int number1 = 10;
+    int number2 = 20;
+    // Operadores Aritméticos
+    // +  Soma
+    // - Subtração
+    // / Divisão
+    // * Multiplicão
+
+  }
+}
