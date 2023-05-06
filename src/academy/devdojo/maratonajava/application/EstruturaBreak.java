2f2e7381a9987511b2feb325a41da81800b2d107
Lucas <lucas.santos@mundowap.com.br>
2023-05-06 12:27:01 -0300

    estrutura de break

--- src/academy/devdojo/maratonajava/application/EstruturaBreak.java
+++ src/academy/devdojo/maratonajava/application/EstruturaBreak.java
@@ -3,14 +3,29 @@ package academy.devdojo.maratonajava.application;
 import java.util.Scanner;
 
 public class EstruturaBreak {
-
   public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      for(int i = 0; i < 50; i++){
-        if(i > 24) break;
-        System.out.println(i);
-      }
-      scanner.close();
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Informe o valor do veículo");
+    double carValue = sc.nextDouble(); // 40.000
+    char resp = 'S';
+    int result;
+    while(resp != 'N'){
+       System.out.println("Informe o valor da parcela");
+       int parcela = sc.nextInt();
+       if(parcela < 1000) {
+         System.out.println("Só é possível parcelar realizar parcelas para veículos com parcela acima 1.000");
+         resp = 'N';
+         break;
+       }
+       result = (int)carValue / parcela;
+       System.out.printf("A condição de parcelamento do veículo  são %d parcelas de %d", result, parcela);
+       break;
+
+    }
+
+
+    sc.close();
   }
+
 
 }
