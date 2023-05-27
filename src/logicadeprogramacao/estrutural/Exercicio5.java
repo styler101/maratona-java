2487360917225e01857badf2154dfb9d8dde70b9
Lucas <lucas.santos@mundowap.com.br>
2023-05-27 12:40:15 -0300

    adicionando o desafio no exercicio5

--- src/logicadeprogramacao/estrutural/Exercicio5.java
+++ src/logicadeprogramacao/estrutural/Exercicio5.java
@@ -1,22 +1,39 @@
 package logicadeprogramacao.estrutural;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Exercicio5 {
     //  Precisamos calcular o valor de uma prestação em atraso.
     // valor da prestação
     // valor do juros.
     // pergar o valor do tempo em dias
+    // Desafio: Receber a data de vencimento e compara com data de hoje para realizar o calcúlo.
     public static void main(String[] args) {
        Scanner scanner = new Scanner(System.in);
+      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+       Date date = new Date();
+       String currentDateFormat = sdf.format(date);
        System.out.println("Informe o valor da prestação");
        double valueToPay = scanner.nextDouble();
        System.out.println("Informe o valor do impost");
        double tax = scanner.nextDouble();
-       System.out.println("Informe a quantos dias se passaram após o vencimento dos mesmo?");
-       int days = scanner.nextInt();
-       double result = valueToPay + (valueToPay * (tax /100)) * days;
-       System.out.println("O valor com os ajustes a seram pagos é : " + result);
+       System.out.println("Informe a data de vencimento da fatura DD/MM/YYYY");
+       scanner.nextLine();
+       String billExpired = scanner.nextLine();
+      try {
+        long diffInMileseconds = Math.abs(sdf.parse(billExpired).getTime() - sdf.parse(currentDateFormat).getTime());
+        long parsedDiffToDays = TimeUnit.DAYS.convert(diffInMileseconds, TimeUnit.MILLISECONDS);
+         double result = valueToPay + (valueToPay * (tax /100)) * parsedDiffToDays;
+         System.out.println("O valor com os ajustes a seram pagos é : " + result);
+
+      } catch (ParseException e) {
+        throw new RuntimeException(e);
+      }
+
        scanner.close();
     }
 
